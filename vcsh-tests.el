e363c061ec128a05775faa0d0196937f2ffd1c3a
Štěpán Němec <stepnem@gmail.com>
2019-10-07 12:59:04 +0200

    vcsh-repo-p test: Don't sort the file list unnecessarily

--- vcsh-tests.el
+++ vcsh-tests.el
@@ -4,7 +4,7 @@
   "Test `vcsh-repo-p'."
   (should-not (vcsh-repo-p "~"))
   (should-not (vcsh-repo-p (vcsh-repo-d)))
-  (let ((repos (directory-files (vcsh-repo-d) t "^[^.]")))
+  (let ((repos (directory-files (vcsh-repo-d) t "^[^.]" t)))
     (dolist (repo repos)
       (should (vcsh-repo-p repo))
       (should (vcsh-repo-p (file-name-as-directory repo)))
